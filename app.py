cd214c248e84cc2b299c2aa2411876ac38672fc7
jivishov <97884055+jivishov@users.noreply.github.com>
2024-09-05 10:16:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -358,7 +358,7 @@ def code_editor_and_prompt():
     response_dict = code_editor(st.session_state.file_content,  buttons=custom_btns, options={"wrap": True}, 
     theme="contrast", height=[30, 50], focus=False, info=info_bar, props={"style": ace_style}, 
     component_props={"style": code_style})
-    st.session_state.file_content = response_dict
+    st.session_state.file_content = response_dict["text"]
     st.write(str(response_dict))
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
