0932943469c157f81300952d754f3d2eeea14fc6
jivishov <97884055+jivishov@users.noreply.github.com>
2024-09-05 22:18:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -407,12 +407,12 @@ def main():
                    with editor_col1:
                         with st.popover("Enter prompt", use_container_width=True):
                             st.session_state.selected_llm = st.selectbox("Choose LLM:", ["Sonnet-3.5", "GPT-4o"])
-                            col1, col2  = st.columns([6, 3])
-                            with col1:
-                                prompt = st.text_area(label="User prompt", label_visibility="collapsed", placeholder="Enter your prompt for code generation and click.", 
+                            #col1, col2  = st.columns([6, 3])
+                            #with col1:
+                            prompt = st.text_area(label="User prompt", label_visibility="collapsed", placeholder="Enter your prompt for code generation and click.", 
                                 height=300)
-                            with col2:
-                                if st.button("Execute prompt", key='exec_prompt'):
+                            #with col2:
+                             if st.button("Execute prompt", key='exec_prompt'):
                                     with st.spinner("Executing your prompt..."):
                                         generated_code = generate_code_with_llm(prompt, st.session_state.file_content)
                                         if generated_code:
