c89c9ab25f288eab229824cbe46136ad85cfe7bd
Emil Jivishov <jivishov@gmail.com>
2024-09-09 21:04:58 -0500

    Update sandbox.py

--- pages/sandbox.py
+++ pages/sandbox.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 if not "sandbox_code" in st.session_state:
-    st.write("No code found to run.")
+    st.write("No code found to execute. Click 'Save to Sandbox' in the bottom right corner to pass your code to sandbox.")
 else:
     exec(st.session_state.sandbox_code)
