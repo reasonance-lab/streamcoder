d6064135393c74ac7a5b37afcd382881378c0683
Emil Jivishov <jivishov@gmail.com>
2024-09-18 09:58:44 -0500

    Update sandbox.py

--- pages/sandbox.py
+++ pages/sandbox.py
@@ -2,7 +2,7 @@ import streamlit as st
 from github import Github, GithubException
 import base64
 from os import environ
-#sandbox file: read and exec a code
+
 def get_file_content(repo, file_path):
     try:
         content = repo.get_contents(file_path)
@@ -19,17 +19,21 @@ def execute_sandbox_code():
     try:
         # Get the repository
         repo = g.get_user().get_repo("streamcoder")
-        
+
         # Read the content of the sandbox.txt file
         file_path = 'pages/sandbox.txt'
         code_content = get_file_content(repo, file_path)
 
         if code_content is not None:
-            # Define a restricted execution environment
+            # Define a partially restricted global execution environment
+            global_env = {
+                "__builtins__": __builtins__,
+                "st": st
+            }
             local_env = {}
             try:
-                # Execute the code within a restricted local environment
-                exec(code_content, {}, local_env)
+                # Execute the code within a partially restricted global environment
+                exec(code_content, global_env, local_env)
                 st.success("Code executed successfully!")
             except Exception as e:
                 st.error(f"Error executing code: {str(e)}")
