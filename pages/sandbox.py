92996a2f0fa462a2e750fc9eb230b1a543a8c80d
Emil Jivishov <jivishov@gmail.com>
2024-09-18 22:00:15 -0500

    Update sandbox.py

--- pages/sandbox.py
+++ pages/sandbox.py
@@ -20,7 +20,6 @@ def preprocess_code(code_content):
     import_lines = []
     other_lines = []
     
-    # Regular expressions to match different import statements
     import_pattern = re.compile(r'^import\s+(.+)$')
     from_import_pattern = re.compile(r'^from\s+(\S+)\s+import\s+(.+)$')
     
@@ -32,7 +31,6 @@ def preprocess_code(code_content):
                 modules = match.group(1).split(',')
                 for module in modules:
                     module = module.strip()
-                    # Handle aliases
                     if ' as ' in module:
                         original, alias = module.split(' as ')
                         import_lines.append(f"{alias.strip()} = custom_import('{original.strip()}')")
@@ -44,7 +42,6 @@ def preprocess_code(code_content):
                 module, attributes = match.groups()
                 attrs = [attr.strip() for attr in attributes.split(',')]
                 for attr in attrs:
-                    # Handle aliases
                     if ' as ' in attr:
                         original, alias = attr.split(' as ')
                         import_lines.append(f"{alias.strip()} = custom_import('{module}').{original.strip()}")
@@ -56,33 +53,24 @@ def preprocess_code(code_content):
     return '\n'.join(import_lines + other_lines)
 
 def execute_sandbox_code():
-    # Authenticate with GitHub
     github_token = environ.get("HUBGIT_TOKEN")
     g = Github(github_token)
     try:
-        # Get the repository
         repo = g.get_user().get_repo("streamcoder")
-        # Read the content of the sandbox.txt file
         file_path = 'pages/sandbox.txt'
         code_content = get_file_content(repo, file_path)
         if code_content is not None:
-            # Preprocess the code
             preprocessed_code = preprocess_code(code_content)
             
-            # Debug: Display the preprocessed code
             st.text_area("Preprocessed Code", preprocessed_code, height=300)
             
-            # Define a partially restricted global execution environment
             global_env = {
                 "__builtins__": __builtins__,
                 "st": st,
                 "custom_import": custom_import
             }
-            # Remove local_env or set it to global_env
             try:
-                # Execute the preprocessed code within a partially restricted global environment
-                exec(preprocessed_code, global_env)  # Use global_env for both globals and locals
-                st.write(global_env)
+                exec(preprocessed_code, global_env)
                 st.success("Code executed successfully!")
             except Exception as e:
                 st.error(f"Error executing code: {str(e)}")
